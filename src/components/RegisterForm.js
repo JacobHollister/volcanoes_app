39c07e4304568482d4f0900dd8a5a0593a7e2c6f
JacobHollister <jacobrhollister@gmail.com>
2022-05-01 15:26:18 +1000

    login and register forms with animations

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,10 +1,80 @@
-import { useEffect, usestate } from "react";
+//import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
 
-export default function RegisterForm () {
+import transition from "../utils/transition"
+
+export default function RegisterForm ({close, isVisible}) {
+    const navigate = useNavigate()
+
+
+    const registerHandler = (e) => {
+        e.preventDefault()
+        close()
+        navigate('/volcanoes')
+    }
+
+    const cancelHandler = (e) => {
+        e.preventDefault()
+        close()
+    }
+
+    const form = (
+        <motion.form
+        key="registerForm"            
+        className="flex flex-col w-full px-20 max-w-lg"
+        initial={{opacity: 0}}
+        animate={{opacity: 1, transition: transition()}}
+        exit={{opacity: 0, transition: transition()}}>
+            <div className="mb-6">
+                <h3 className="font-serif text-3xl text-center mb-4">
+                    Register
+                </h3>
+                <label 
+                    className="block ml-1 mb-2 text-xl font-serif font-medium text-gray-900 dark:text-gray-300"
+                    htmlFor="email">
+                        Email
+                </label>
+                <input 
+                    required
+                    type="email" 
+                    id="email" 
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                    />
+            </div>
+            <div className="mb-6">
+                <label 
+                    className="block ml-1 mb-2 text-xl font-serif text-gray-900 dark:text-gray-300"
+                    htmlFor="password">
+                        Password
+                    </label>
+                <input 
+                    required
+                    type="password" 
+                    id="password" 
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                    />
+            </div>
+            <div className="flex flex-row gap-10 px-1">
+                <button 
+                    className="bg-blue-500 hover:bg-blue-700 font-medium text-white py-1 px-4 rounded h-8 flex-1"
+                    onClick={(e) => registerHandler(e)}
+                    >
+                        Register
+                </button>
+                <button 
+                    className="bg-slate-500 hover:bg-slate-400 font-medium text-white py-1 px-4 rounded h-8 flex-1"
+                    onClick={(e) => cancelHandler(e)}
+                    >
+                        Cancel
+                </button>
+            </div>
+        </motion.form>
+    )
 
     return(
-        <form>
-            Register Form
-        </form>
+        <>
+            {isVisible ? form : null}
+        </>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,29 +13,40 @@ export default function Home () {
     const navigate = useNavigate()
 
     const [actionContent, setActionContent] = useState('home')
+    const [loginVisible, setLoginVisible] = useState(false)
+    const [registerVisible, setRegisterVisible] = useState(false)
     const [contentSize, setContentSize] = useState("200px")
 
-    const loginButtonHandler = () => {
+    const showLoginForm = () => {
         setContentSize("500px")
+        setLoginVisible(true)
         setActionContent('login')
     }
 
-    const registerButtonHandler = () => {
+    const showRegisterForm = () => {
         setContentSize("500px")
+        setRegisterVisible(true)
         setActionContent('register')
     }
 
-    const cancelButtonHandler = () => {
+    const closeFormHandler = () => {
         setContentSize("200px")
+        setLoginVisible(false)
         setActionContent('home')
+    }
+
+    const guestButtonHandler = () => {
+        setLoginVisible(false)
+        setActionContent(null)
+        navigate("/volcanoes")
     }
 
     let formContainer;
 
     if( actionContent === "register" ){
-        formContainer = (<RegisterForm cancel={cancelButtonHandler}/>)
+        formContainer = (<RegisterForm close={closeFormHandler} isVisible={registerVisible}/>)
     } else if ( actionContent === 'login' ) {
-        formContainer = (<LoginForm cancel={cancelButtonHandler}/>)
+        formContainer = (<LoginForm close={closeFormHandler} isVisible={loginVisible}/>)
     } else if ( actionContent === 'home' ){
         formContainer = (                    
             <motion.div 
@@ -46,15 +57,15 @@ export default function Home () {
                 exit={{opacity: 0, transition: transition(0)}}>
                 <button 
                     className="mt-auto mb-3 w-56 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                    onClick={() => loginButtonHandler()}
+                    onClick={() => showLoginForm()}
                     >Login</button>
                 <button 
                     className="mb-3 w-56 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" 
-                    onClick={() => registerButtonHandler()}
+                    onClick={() => showRegisterForm()}
                     >Register</button>
                 <button 
                     className="mb-auto w-56 bg-white hover:bg-gray-200 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
-                    onClick={() => navigate("/volcanoes")}
+                    onClick={() => guestButtonHandler()}
                     >Continue as guest</button>
             </motion.div>
             )
@@ -100,7 +111,7 @@ export default function Home () {
                             >
                             <AnimatePresence exitBeforeEnter>
                                 {formContainer} 
-                            </AnimatePresence> 
+                            </AnimatePresence>
                         </div>
                     
             </motion.div>
